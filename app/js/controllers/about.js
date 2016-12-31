656147b5b70d96611f28217da1def97ed10e6d81
Juan Arribillaga <juan.arribillaga@globant.com>
2016-12-31 13:51:14 -0300

    Partially api connect implementation.

--- app/js/controllers/about.js
+++ app/js/controllers/about.js
@@ -1,10 +1,27 @@
-function AboutCtrl() {
+function AboutCtrl(ApiConnect) {
+	'ngInject';
 
   // ViewModel
   const vm = this;
 
+  vm.getCollaborators = () => {
+  	let userData = {};
+  	
+  	ApiConnect.getRequest('https://api.github.com/repos/octokit/octokit.rb')
+  		.then((response) => { userData = response.data;})
+  		.catch((response) => { console.error('Ooops, something wen\'t wrong getting User Data', response) });
+
+  	ApiConnect.getRequest(userData.collaborators_url)
+  		.then((response) => {console.log(response.data)})
+  		.catch((response) => { console.error('Ooops, something wen\'t wrong getting', response) });
+  };
+
+
+
   vm.title = 'About US page ...';
   vm.description = 'Some text here ...';
+
+  
 }
 
 export default {
---
+++ app/js/services/api-connect.js
@@ -0,0 +1,28 @@
+function ApiConnect($http) {
+  'ngInject';
+
+  const service = {};
+
+  /**
+   * Simple service method for http requests (promises).
+   * @param {String} url
+   * @returns {Promise}
+   */
+
+  service.getRequest = (url, type, data) => {
+    console.log(url);
+    return $http({
+        method: type || 'GET',
+        url: url,
+        data: data ? data : '',
+        cache: false
+    });
+  };
+
+  return service;
+}
+
+export default {
+  name: 'ApiConnect',
+  fn: ApiConnect
+};
